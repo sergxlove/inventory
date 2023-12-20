484d3d9b05d109113f70b4cb025a5d4d1bd943a6
sergxlove <sergxlove@mail.ru>
2023-12-20 21:30:11 +0600

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+class inventory
+{
+public:
+private:
+};
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	return 0;
+}
